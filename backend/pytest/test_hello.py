8a8088179c2f5eca2f51834f1b9ff2f4c1659564
GabrielBrancdCast <gabriel@agenciabrandcast.com.br>
2026-05-12 16:18:34 -0300

    refat: melhora nos testes

--- backend/pytest/test_hello.py
+++ backend/pytest/test_hello.py
@@ -1,3 +1,14 @@
-def test_hello():
-    assert True
 
+from fastapi import FastAPI 
+from fastapi.cli import TestClient
+from main import app
+
+
+
+
+
+def test_hello_world():
+
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
